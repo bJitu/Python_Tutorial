08bcea4e73820323d41c6f40da43912c819ae9fb
Jitu Biswakarma <jitub299@gmail.com>
2020-07-09 20:01:18 +0530

    optimized

--- WordDictionary_cmd_line/word_dictionary.py
+++ WordDictionary_cmd_line/word_dictionary.py
@@ -6,13 +6,19 @@ data = {k.lower(): v for k, v in data.items()}
 
 
 def translate(w):
+    """
+    Gives out the meaning of a word from the data file
+    :param w: the word for which you want to get the meaning
+    :return: either meaning or message string
+    """
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+    matches = get_close_matches(w, data.keys())
+    if len(matches) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % matches[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
+            return data[matches[0]]
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
